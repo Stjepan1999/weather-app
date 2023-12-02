0032a3c1e676ee21aa3f4c0bcdec05fbe032d33f
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-12-02 11:05:15 +0100

    Set up project

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-param-reassign': ['error', { props: false }],
+    'no-use-before-define': ['error', { functions: false }],
+    'no-plusplus': 'off',
+    'import/no-cycle': 'off',
+    'no-console': 'off',
+  },
+};
