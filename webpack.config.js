9b5562676658cd81eecac0f018e6dbb782aa2c58
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-12-22 16:47:04 +0100

    Update webpack configuratoin for environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: './src/index.js',
+    devtool: isDevelopment ? 'inline-source-map' : false,
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+  };
 };
